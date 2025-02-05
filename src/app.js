0f77d0f5fd3551db1141bf904ea3f929b823e9f5
GiG <g.gargaro@gmail.com>
2025-02-05 16:09:09 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const App = () => {
   const [onlineTransaction, setOnlineTransaction] = useState(false);
   const [message, setMessage] = useState('');
 
-  const endpoint = "http://ai-serve-model-mediator.frauddetection-gig.svc.cluster.local:8080";
+  const endpoint = "http://ai-serve-model-proxy.frauddetection-gig.svc.cluster.local:8080";
 
   const handleSubmit = async (event) => {
     event.preventDefault();
